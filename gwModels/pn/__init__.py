d039937d4ca0a04017d1c6394422447e99e76c1b
tousifislam <tousifislam24@gmail.com>
2024-11-03 14:32:36 -0800

    adding more

--- gwModels/pn/__init__.py
+++ gwModels/pn/__init__.py
@@ -1,3 +1,5 @@
 from .pn_utils import *
 from . import non_spinning_circular
 from .non_spinning_circular import Blanchet2024
+from ..utils import mass_ratio_to_symmetric_mass_ratio
+from . import ecc_nospin
