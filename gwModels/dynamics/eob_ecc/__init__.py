feb0f3ca26371743463d6982ab301ee1acccf862
tousifislam <tousifislam24@gmail.com>
2026-03-27 10:14:19 +0530

    Rename dynamics files: nonprec_secular.py and nonprec_postadiabatic.py

--- gwModels/dynamics/eob_ecc/__init__.py
+++ gwModels/dynamics/eob_ecc/__init__.py
@@ -1,10 +1,10 @@
-from .eob_ecc_nonprec_dynamics import (
+from .nonprec_secular import (
     edot_resum,
     xdot_resum,
     zetadot_func,
     integrate_eob_eccentric_dynamics,
 )
-from .postadiabatic import (
+from .nonprec_postadiabatic import (
     pa_correction_x,
     pa_correction_e,
     pa_correction_zeta,
