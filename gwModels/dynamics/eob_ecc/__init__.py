7e49f2b023c322a0e9fe4e3fd8e8db90ab26bd90
tousifislam <tousifislam24@gmail.com>
2026-03-27 10:11:47 +0530

    Move EOB eccentric dynamics into dynamics/eob_ecc submodule

---
+++ gwModels/dynamics/eob_ecc/__init__.py
@@ -0,0 +1,12 @@
+from .eob_ecc_nonprec_dynamics import (
+    edot_resum,
+    xdot_resum,
+    zetadot_func,
+    integrate_eob_eccentric_dynamics,
+)
+from .postadiabatic import (
+    pa_correction_x,
+    pa_correction_e,
+    pa_correction_zeta,
+    apply_pa_corrections,
+)
